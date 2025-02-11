ec0f08d9a8d92f23c8699ace6ca20beb887fd132
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-11 09:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// Car Constructor Function
-function Car(make, model) {
-    this.make = make;
-    this.model = model;
+// Car class
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    // Method to get make and model
+    getMakeModel() {
+        return `${this.make} ${this.model}`;
+    }
 }
 
-// Adding method to Car prototype
-Car.prototype.getMakeModel = function () {
-    return `${this.make} ${this.model}`;
-};
+// SportsCar class inheriting from Car
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); // Call parent class constructor
+        this.topSpeed = topSpeed;
+    }
 
-// SportsCar Constructor Function (Inheriting from Car)
-function SportsCar(make, model, topSpeed) {
-    // Call the Car constructor to initialize make and model
-    Car.call(this, make, model);
-    this.topSpeed = topSpeed;
+    // Method to get top speed
+    getTopSpeed() {
+        return this.topSpeed;
+    }
 }
-
-// Inherit from Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
-
-// Restore the constructor reference to SportsCar
-SportsCar.prototype.constructor = SportsCar;
-
-// Adding method to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-    return this.topSpeed;
-};
 
 // Do not change the code below
 window.Car = Car;
